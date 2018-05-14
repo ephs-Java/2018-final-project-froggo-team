39c0336cce0169f3dd668495ed59debfba7e440f
64003084 <64003084@hs-03900-macair.ad.edenpr.org>
2018-05-14 16:09:31 -0500

    n

--- src/Froggo/Frogger.java
+++ src/Froggo/Frogger.java
@@ -15,13 +15,17 @@ public class Frogger implements ActionListener {
 
 	public static Frogger frogger;
 
-	public final int WIDTH = 800, HEIGHT = 800;
+	public final int WIDTH = 1200, HEIGHT = 800;
 
 	public Renderer renderer;
 
 	public Rectangle frog;
 
+	public int ticks, xMotion, yMotion;
+
 	public ArrayList<Rectangle> car;
+
+	public boolean gameOver, started;
 
 	public Random rand;
 
@@ -43,17 +47,28 @@ public class Frogger implements ActionListener {
 		frog = new Rectangle(WIDTH / 2 - 10, HEIGHT / 2 - 10, 20, 20);
 		car = new ArrayList<Rectangle>();
 
+		addCar(true);
+		addCar(true);
+		addCar(true);
+		addCar(true);
+
 		timer.start();
 
 	}
 
-	public void addCar() {
+	public void addCar(boolean start) {
 
 		int space = 300;
 		int width = 100;
 		int height = 50 + rand.nextInt(300);
 
-		car.add(new Rectangle(WIDTH + width + car.size() * 300, HEIGHT - height));
+		if (start) {
+			car.add(new Rectangle(WIDTH + width + car.size() * 300, HEIGHT - height - 120, width, height));
+			car.add(new Rectangle(WIDTH + width + (car.size() - 1) * 300, 0, width, HEIGHT - height - space));
+		} else {
+			car.add(new Rectangle(car.get(car.size() - 1).x + 600, HEIGHT - height - 120, width, height));
+			car.add(new Rectangle(car.get(car.size() - 1).x, 0, width, HEIGHT - height - space));
+		}
 
 	}
 
@@ -67,6 +82,46 @@ public class Frogger implements ActionListener {
 	@Override
 	public void actionPerformed(ActionEvent e) {
 
+		int speed = 10;
+
+		ticks++;
+
+		if (started) {
+			for (int i = 0; i < car.size(); i++) {
+				Rectangle Car = car.get(i);
+				Car.x -= speed;
+			}
+
+			if (ticks % 2 == 0 && yMotion < 15 && xMotion < 15) {
+				yMotion += 2;
+				xMotion += 2;
+			}
+
+			for (int i = 0; i < car.size(); i++) {
+				Rectangle Car = car.get(i);
+
+				if (Car.x + Car.width < 0) {
+					car.remove(Car);
+
+					if (Car.y == 0) {
+						addCar(false);
+					}
+				}
+			}
+
+			frog.y += yMotion;
+			frog.x += xMotion;
+
+			for (Rectangle Car : car) {
+				if (Car.intersects(frog)) {
+					gameOver = true;
+				}
+			}
+
+			if (frog.y > HEIGHT - 120) {
+				gameOver = true;
+			}
+		}
 		renderer.repaint();
 	}
 
@@ -80,6 +135,10 @@ public class Frogger implements ActionListener {
 
 		g.setColor(Color.green);
 		g.fillRect(frog.x, frog.y, frog.width, frog.height);
+
+		for (Rectangle Car : car) {
+			paintCar(g, Car);
+		}
 	}
 
 	public static void main(String[] args) {
