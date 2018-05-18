5e396f1c6267ea7f908aa4c02a874d739ef1ba2c
90303199 <90303199@hs-06910-macair.ad.edenpr.org>
2018-05-18 08:28:41 -0500

    lol

--- src/Froggo/Frogger.java
+++ src/Froggo/Frogger.java
@@ -1,17 +1,10 @@
 package Froggo;
 
 import java.awt.Color;
-import java.awt.Font;
 import java.awt.Graphics;
-import java.awt.Rectangle;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
-import java.util.ArrayList;
-import java.util.Random;
 import javax.swing.JFrame;
-import javax.swing.Timer;
 
-public class Frogger implements ActionListener {
+public class Frogger {
 
 	public static Frogger frogger;
 
